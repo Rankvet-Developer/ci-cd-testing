6df4788900895ac4f07e5cc8efa4286fe5e12f56
Rankvet-Developer <development@rankvet.com>
2023-03-30 14:40:08 +0600

    initialized the testing server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/",(req,res) => {
+    res.send("hello from testing express server");
+})
+
+
+
+app.listen(5000,() => {
+    console.log("listening from 5000 port");
+})
